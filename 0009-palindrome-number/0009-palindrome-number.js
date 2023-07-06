2eb15b6e60ec11ce22aaa6c7087896a28f5ddcee
Moon <faris.kocak@gmail.com>
2023-07-07 02:47:56 +0700

    Time: 158 ms (92.73%), Space: 50.8 MB (74.17%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,17 +3,17 @@
  * @return {boolean}
  */
 var isPalindrome = function (x) {
-    // Make number to array
-    var num = String(x).split("").map((x) => {
-        return Number(x)
-    })
-    // Shallow array because reverse() changes the original array
-    var rev = [...num].reverse()
-    console.log(rev + " and " + num)
-    for (let i = 0; i < rev.length; i++) {
-        if (num[i] != rev[i]) {
-            return false
-        }
+    if (x < 0) {
+        return false; // Negative numbers are not palindromes
     }
-    return true
+
+    var num = x;
+    var reversed = 0;
+
+    while (num > 0) {
+        reversed = reversed * 10 + (num % 10);
+        num = Math.floor(num / 10);
+    }
+
+    return x === reversed;
 };
