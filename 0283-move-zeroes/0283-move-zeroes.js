feeb437eac2011e3dfd72d14e1afec29b7989ee0
Moon <faris.kocak@gmail.com>
2023-07-30 18:33:56 +0700

    Time: 77 ms (95.77%), Space: 47 MB (26.06%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+    let left = 0
+    let right = left + 1
+
+    while (right <= nums.length - 1) {
+        if (nums[left] !== 0) {
+            left++
+            right++
+        }
+        else {
+            if (nums[right] !== 0) {
+                [nums[left], nums[right]] = [nums[right], nums[left]]
+                left++
+            }
+            right++
+        }
+    }
+};
