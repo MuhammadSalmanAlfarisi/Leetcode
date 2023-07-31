1e52dfb3b6ccfdfde524b75934b04e9831761e18
Moon <faris.kocak@gmail.com>
2023-07-31 08:41:50 +0700

    Time: 51 ms (99.59%), Space: 45.1 MB (20.76%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -4,7 +4,9 @@
  */
 var removeDuplicates = function (nums) {
     let left = 0
+    // using two pointers to compare each indexes once
     for (let right = 1; right < nums.length; right++) {
+        // if the next value != prev value, then shift the left pointer
         if (nums[right] !== nums[left]) {
             left++
             nums[left] = nums[right]
