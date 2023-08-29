9c2fbf0246d31b4a7c9cc13dac98d87d35cc1dbd
Moon <faris.kocak@gmail.com>
2023-08-30 02:16:05 +0700

    Time: 86 ms (45.86%), Space: 41.5 MB (98.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,21 +3,12 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
-    const numMap = new Map(); // Create a Map to store values and indices
-
-    for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-
-        if (numMap.has(complement)) {
-            // Return the indices of the two numbers
-            return [numMap.get(complement), i];
+var twoSum = function(nums, target) { 
+    for (let l = 0; l < nums.length-1; l++){
+        for (let r = l+1; r < nums.length; r++){
+            if (nums[r] + nums[l] === target){
+                return [l, r]
+            }
         }
-
-        // Add the current number and its index to the map
-        numMap.set(nums[i], i);
     }
-
-    // No solution found
-    return [];
-}
+};
